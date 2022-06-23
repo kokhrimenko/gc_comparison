4f14ad5d744395a60f58b659deb78cc945e6eee5
Kostiantyn Okhrimenko <konstantin.okhrimenko@asml.com>
2022-06-23 22:38:03 +0200

    add tester scripts and JMX support

---
+++ src/main/resources/scripts/run_g1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Staring ping of the web endpoint for G1 garbage collector"
+
+while true
+do
+        response=$(curl --write-out %{time_total} --silent --output /dev/null 172.20.80.1:8080/get-version)
+
+        if [[ "$response" > 0.1 ]] ; then
+                echo "long running request, took: $response seconds"
+        fi
+done
